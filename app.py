d59b007b6836aeab93cc3ef9bfab5e2723bf6c44
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2025-01-18 15:51:59 +0200

    2nd

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # Specifies the interpreter to be used for this script (Python 3)
 
 from application import app
+# Imports the 'app' object from the 'application' module
 
 
 if __name__ == '__main__':
