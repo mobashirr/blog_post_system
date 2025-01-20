1cf0711fa0c1dcdddb017fd698a8d2de26559d20
GadourGuy <140702800+GadourGuy@users.noreply.github.com>
2025-01-20 11:15:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/ python3
-# Specifies the interpreter to be used for this script (Python 3)
+# Specifies the interpreter to be used for this script (Python 3
 
 from application import app
 # Imports the 'app' object from the 'application' module
